ff142d7bf98b1b6d4332821d475a47325c62c733
Christopher Kemp <cmkemp52@gmail.com>
2019-08-30 15:41:50 -0400

    Committing today's exercises

---
+++ hello2.py
@@ -0,0 +1,3 @@
+name = input('What is your name? '.upper())
+print("HELLO "+name.upper()+"!")
+print(name.upper()+"\'S NAME HAS "+str(len(name))+" CHARACTERS!")
---
+++ madlib.py
@@ -0,0 +1,7 @@
+print("Please fill in the blanks below")
+print("______'s favorite subject in school is _____")
+
+name = input('Please give a name: ')
+subj = input('Please give a subject: ')
+
+print(name +"'s favorite subject in school is "+subj)
